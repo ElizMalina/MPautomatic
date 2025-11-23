63912372f190fefd087aeb516af33fb1dd89de00
Elizaveta Malina <elizavetamalina8975@gmail.com>
2025-11-24 01:01:40 +0300

    Update MPautomatic.cpp

--- src/MPautomatic.cpp
+++ src/MPautomatic.cpp
@@ -1,17 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include "MPautomatic.h"
 
 
-
-/*int main(){
-
-    std::string input;
-    std::cin>>input;
-    
-    MPautomatic mp(input);
-    mp.process();
-
-    return 0;
-}*/
